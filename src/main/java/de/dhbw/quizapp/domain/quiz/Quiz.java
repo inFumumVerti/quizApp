cd918fdeb5c3331dfa7b99296aa0e857aa4a3051
inFumumVerti <linus.pust@gmail.com>
2023-03-20 17:01:51 +0100

    wrote Quiz Class

--- src/main/java/de/dhbw/quizapp/domain/quiz/Quiz.java
+++ src/main/java/de/dhbw/quizapp/domain/quiz/Quiz.java
@@ -1,37 +1,59 @@
 package de.dhbw.quizapp.domain.quiz;
 
 import de.dhbw.quizapp.domain.question.Question;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "quiz")
 public class Quiz {
 
-    private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private UUID id;
+
+    @NotBlank
+    @Column(name = "title")
     private String title;
+
+    @NotBlank
+    @Column(name = "description")
     private String description;
+
+    @ManyToMany
     private List<Question> questions;
 
-    public Quiz(Long id, String title, String description) {
-        this.id = id;
+    protected Quiz() {
+        // default constructor for JPA
+    }
+
+    public Quiz(@NotBlank final String title, @NotBlank final String description) {
+        this.id = UUID.randomUUID();
         this.title = title;
         this.description = description;
         this.questions = new ArrayList<>();
     }
 
-    public Long getId() {
+    public UUID getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(final String title) {
         this.title = title;
     }
 
@@ -39,7 +61,7 @@ public class Quiz {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(final String description) {
         this.description = description;
     }
 
@@ -47,15 +69,11 @@ public class Quiz {
         return questions;
     }
 
-    public void setQuestions(List<Question> questions) {
-        this.questions = questions;
-    }
-
-    public void addQuestion(Question question) {
+    public void addQuestion(@Nonnull final Question question) {
         this.questions.add(question);
     }
 
-    public void removeQuestion(Question question) {
+    public void removeQuestion(@Nonnull final Question question) {
         this.questions.remove(question);
     }
 }
