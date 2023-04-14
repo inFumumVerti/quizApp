043d4058a7fd9366432da0a9d4342f91554a524f
inFumumVerti <linus.pust@gmail.com>
2023-04-14 14:14:11 +0200

    Update PlayQuiz.js to be more user-friendly

--- geo-quiz/src/components/PlayQuiz.js
+++ geo-quiz/src/components/PlayQuiz.js
@@ -1,3 +1,4 @@
+// PlayQuiz.js
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
@@ -5,7 +6,7 @@ import './PlayQuiz.css';
 
 const PlayQuiz = () => {
     const [quiz, setQuiz] = useState(null);
-    const [userAnswers, setUserAnswers] = useState([]);
+    const [userAnswers, setUserAnswers] = useState({ currentIndex: 0, answers: [] });
     const [showResults, setShowResults] = useState(false);
     const { id } = useParams();
     const QUIZ_API_BASE_URL = 'http://localhost:8080/api/quiz';
@@ -13,12 +14,9 @@ const PlayQuiz = () => {
     useEffect(() => {
         (async function fetchQuiz() {
             try {
-                alert("aaa");
                 const response = await axios.get(`${QUIZ_API_BASE_URL}/${id}`);
-                alert("aaaaa");
-                alert(JSON.stringify(response.data, null, 2)); // Add this line
                 setQuiz(response.data);
-                setUserAnswers(new Array(response.data.questions.length).fill(null));
+                setUserAnswers({ ...userAnswers, answers: new Array(response.data.questions.length).fill(null) });
             } catch (error) {
                 console.error('Error fetching quiz:', error);
             }
@@ -26,19 +24,22 @@ const PlayQuiz = () => {
     }, [id]);
 
     const handleAnswerChange = (questionIndex, answer) => {
-        const newUserAnswers = [...userAnswers];
+        const newUserAnswers = [...userAnswers.answers];
         newUserAnswers[questionIndex] = answer;
-        setUserAnswers(newUserAnswers);
+        setUserAnswers({ ...userAnswers, answers: newUserAnswers });
     };
 
     const handleSubmit = () => {
-        setShowResults(true);
+        if (userAnswers.currentIndex === quiz.questions.length - 1) {
+            setShowResults(true);
+        } else {
+            setUserAnswers({ ...userAnswers, currentIndex: userAnswers.currentIndex + 1 });
+        }
     };
 
     const calculateScore = () => {
-        return userAnswers.reduce(
-            (score, answer, index) =>
-                quiz.questions[index].correctAnswer === answer ? score + 1 : score,
+        return userAnswers.answers.reduce(
+            (score, answer, index) => (quiz.questions[index].correctAnswer === answer ? score + 1 : score),
             0
         );
     };
@@ -50,49 +51,106 @@ const PlayQuiz = () => {
     if (showResults) {
         return (
             <div className="quiz-results">
-                <h2>Results</h2>
+                <h1>Results</h1>
                 <p>
                     You scored {calculateScore()} out of {quiz.questions.length}
                 </p>
                 <Link to="/quizList">Back to quiz list</Link>
+                <div className="result-question-container">
+                    {quiz.questions.map((question, questionIndex) => {
+                        const isUserCorrect = question.correctAnswer === userAnswers.answers[questionIndex];
+                        return (
+                            <div key={question.id} className="question">
+                                <h3>
+                                    {question.title}
+                                    <span
+                                        className={`result-indicator ${isUserCorrect ? 'correct' : 'incorrect'}`}
+                                    >
+                                {isUserCorrect ? 'Correct answer' : 'Incorrect answer'}
+                            </span>
+                                </h3>
+                            <div className="answer-grid result-question-answer-grid">
+                                {question.answerOptions.map((answer, answerIndex) => {
+                                    const answerCode = String.fromCharCode(65 + answerIndex);
+                                    const isCorrectAnswer = question.correctAnswer === answerCode;
+                                    const isUserAnswer = userAnswers.answers[questionIndex] === answerCode;
+                                    const isUserCorrect = isCorrectAnswer && isUserAnswer;
+
+                                    let answerStyle = '';
+                                    if (isUserCorrect) {
+                                        answerStyle = 'correct-answer user-answer';
+                                    } else if (isCorrectAnswer) {
+                                        answerStyle = 'correct-answer';
+                                    } else if (isUserAnswer) {
+                                        answerStyle = 'incorrect-answer user-answer';
+                                    }
+
+                                    return (
+                                        <div
+                                            key={answerIndex}
+                                            className={`answer-cell answer-${answerIndex} ${answerStyle}`}
+                                        >
+                                            {answer}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                            );
+                    })}
+                </div>
             </div>
         );
-    } else {
+    }
+
+
+    else {
+        const question = quiz.questions[userAnswers.currentIndex];
         return (
             <div className="play-quiz">
                 <h2>{quiz.title}</h2>
                 <p>{quiz.description}</p>
-                {quiz.questions.map((question, questionIndex) => (
-                    <div key={question.id} className="question">
-                        <h3>{question.title}</h3>
-                        <ul>
-                            {question.answerOptions.map((answer, answerIndex) => (
-                                <li key={answerIndex}>
-                                    <input
-                                        type="radio"
-                                        id={`question-${question.id}-answer-${answerIndex}`}
-                                        name={`question-${question.id}`}
-                                        value={String.fromCharCode(65 + answerIndex)}
-                                        onChange={() =>
-                                            handleAnswerChange(questionIndex, String.fromCharCode(65 + answerIndex))
-                                        }
-                                        checked={
-                                            userAnswers[questionIndex] ===
-                                            String.fromCharCode(65 + answerIndex)
-                                        }
-                                    />
-                                    <label htmlFor={`question-${question.id}-answer-${answerIndex}`}>
-                                        {answer}
-                                    </label>
-                                </li>
-                            ))}
-                        </ul>
+                <div key={question.id} className="question">
+                    <h3>{question.title}</h3>
+                    <div className="answer-grid">
+                        {question.answerOptions.map((answer, answerIndex) => (
+                            <label
+                                key={answerIndex}
+                                htmlFor={`question-${question.id}-answer-${answerIndex}`}
+                                className={`answer-cell answer-${answerIndex} ${
+                                    userAnswers.answers[userAnswers.currentIndex] === String.fromCharCode(65 + answerIndex)
+                                        ? 'selected-answer'
+                                        : ''
+                                }`}
+                            >
+                                <input
+                                    type="radio"
+                                    id={`question-${question.id}-answer-${answerIndex}`}
+                                    name={`question-${question.id}`}
+                                    value={String.fromCharCode(65 + answerIndex)}
+                                    onChange={() =>
+                                        handleAnswerChange(userAnswers.currentIndex, String.fromCharCode(65 + answerIndex))
+                                    }
+                                    checked={
+                                        userAnswers.answers[userAnswers.currentIndex] === String.fromCharCode(65 + answerIndex)
+                                    }
+                                />
+                                {answer}
+                            </label>
+                        ))}
                     </div>
-                ))}
-                <button onClick={handleSubmit}>Submit</button>
+                </div>
+                <button
+                    onClick={handleSubmit}
+                    disabled={userAnswers.answers[userAnswers.currentIndex] === null}
+                >
+                    {userAnswers.currentIndex === quiz.questions.length - 1 ? 'Submit' : 'Next'}
+                </button>
             </div>
         );
     }
 };
 
 export default PlayQuiz;
+
+
