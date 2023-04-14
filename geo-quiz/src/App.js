dde93e133f20f8f88fe2dbb2043fb749de30f490
inFumumVerti <linus.pust@gmail.com>
2023-04-14 07:54:43 +0200

    General updates, updated App.js to reflect new functions

--- geo-quiz/src/App.js
+++ geo-quiz/src/App.js
@@ -1,50 +1,50 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import QuizList from './components/QuizList';
-import Quiz from './components/Quiz';
 import AddQuiz from './components/AddQuiz';
+import PlayQuiz from './components/PlayQuiz';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <nav>
-        <ul className="top-bar">
-          <li>
-            <Link to="/">geoo!</Link>
-          </li>
-          <li>
-            <Link to="/quizList">QuizList</Link>
-          </li>
-          <li>
-            <Link to="/add-quiz">Add Quiz</Link>
-          </li>
-        </ul>
-      </nav>
+    return (
+        <Router>
+            <nav>
+                <ul className="top-bar">
+                    <li>
+                        <Link to="/">geoo!</Link>
+                    </li>
+                    <li>
+                        <Link to="/quizList">QuizList</Link>
+                    </li>
+                    <li>
+                        <Link to="/add-quiz">Add Quiz</Link>
+                    </li>
+                </ul>
+            </nav>
 
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/quizList" element={<QuizList />} />
-          <Route path="/quiz/:id" element={<Quiz />} />
-          <Route path="/add-quiz" element={<AddQuiz />} />
-        </Routes>
-      </div>
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/quizList" element={<QuizList />} />
+                    <Route path="/add-quiz" element={<AddQuiz />} />
+                    <Route path="/quiz/:id" element={<PlayQuiz />} /> {/* Add this line */}
+                </Routes>
+            </div>
 
-      <div className="bottom-bar">
-        <p>&copy; 2023 Quiz App</p>
-      </div>
-    </Router>
-  );
+            <div className="bottom-bar">
+                <p>&copy; 2023 Quiz App</p>
+            </div>
+        </Router>
+    );
 }
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to the Quiz App</h1>
-      <p>Test your knowledge with our quizzes!</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to the Quiz App</h1>
+            <p>Test your knowledge with our quizzes!</p>
+        </div>
+    );
 };
 
 export default App;
--- geo-quiz/src/components/Quiz.js
+++ geo-quiz/src/components/Quiz.js
@@ -1,31 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Question from './Question';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import PlayQuiz from './PlayQuiz';
 
-const Quiz = ({ match }) => {
-  const [quiz, setQuiz] = useState(null);
+const Quiz = () => {
+    const { id } = useParams();
 
-  useEffect(() => {
-    const fetchQuiz = async () => {
-      const response = await axios.get(`/api/quiz/${match.params.id}`);
-      setQuiz(response.data);
-    };
-    fetchQuiz();
-  }, [match.params.id]);
-
-  if (!quiz) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <h1>{quiz.title}</h1>
-      <p>{quiz.description}</p>
-      {quiz.questions.map((question) => (
-        <Question key={question.id} question={question} />
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            <PlayQuiz id={id} />
+        </div>
+    );
 };
 
 export default Quiz;
