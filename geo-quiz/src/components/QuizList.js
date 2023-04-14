07674450fd2ed8d37a8e9234da7f067bea027b0d
inFumumVerti <linus.pust@gmail.com>
2023-04-14 07:53:40 +0200

    QuizList now in working order

--- geo-quiz/src/components/QuizList.js
+++ geo-quiz/src/components/QuizList.js
@@ -1,30 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './QuizList.css'; // Add this line
+import axios from "axios";
 
 const QuizList = () => {
-  const [quizzes, setQuizzes] = useState([]);
+    const [quizzes, setQuizzes] = useState([]);
+    const QUIZ_API_BASE_URL = "http://localhost:8080/api/quiz";
 
-  useEffect(() => {
-    const fetchQuizzes = async () => {
-      const response = await axios.get('/api/quiz');
-      setQuizzes(response.data);
-    };
-    fetchQuizzes();
-  }, []);
+    useEffect(() => {
+        (async function fetchQuizzes() {
+            try {
+                const response = await axios.get(QUIZ_API_BASE_URL);
+                setQuizzes(response.data);
+            } catch (error) {
+                console.error('Error fetching quizzes:', error);
+            }
+        })();
+    }, []);
 
-  return (
-    <div>
-      <h1>Quizzes</h1>
-      <ul>
-        {quizzes.map((quiz) => (
-          <li key={quiz.id}>
-            <Link to={`/quiz/${quiz.id}`}>{quiz.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+
+    return (
+        <div className="quiz-list">
+            <h2>Available Quizzes</h2>
+            <ul>
+                {quizzes.map((quiz) => (
+                    <li key={quiz.id}>
+                        <Link to={`/quiz/${quiz.id}`}>
+                            <h3>{quiz.title}</h3>
+                        </Link>
+                        <p>{quiz.description}</p>
+                        <p>
+                            <strong>Number of questions:</strong> {quiz.questions.length}
+                        </p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default QuizList;
