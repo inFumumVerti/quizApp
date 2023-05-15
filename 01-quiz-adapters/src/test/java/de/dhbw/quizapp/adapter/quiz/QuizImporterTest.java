0ad3e2899e0d11390023a511e96a8cea8a03e82b
inFumumVerti <linus.pust@gmail.com>
2023-05-15 20:49:50 +0200

    Added adequate testing for QuizImporter class

---
+++ 01-quiz-adapters/src/test/java/de/dhbw/quizapp/adapter/quiz/QuizImporterTest.java
@@ -0,0 +1,76 @@
+package de.dhbw.quizapp.adapter.quiz;
+
+import de.dhbw.quizapp.domain.quiz.Quiz;
+import de.dhbw.quizapp.domain.question.Question;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class QuizImporterTest {
+    private QuizImporter quizImporter;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        objectMapper = new ObjectMapper();
+        quizImporter = new QuizImporter(objectMapper);
+    }
+
+    @Test
+    public void testCreateQuizFromValidJson() {
+        String json = "{\"title\": \"Test Quiz\", \"description\": \"This is a test quiz.\", \"questions\": [{\"title\": \"Test Question\", \"correctAnswer\": \"A\", \"answerOptions\": [\"Option A\", \"Option B\", \"Option C\", \"Option D\"]}]}";
+
+        try {
+            Quiz quiz = quizImporter.createQuizFromJson(json);
+
+            assertEquals("Test Quiz", quiz.getTitle());
+            assertEquals("This is a test quiz.", quiz.getDescription());
+            assertEquals(1, quiz.getQuestions().size());
+
+            Question question = quiz.getQuestions().get(0);
+            assertEquals("Test Question", question.getTitle());
+            assertEquals("A", question.getCorrectAnswer());
+            assertEquals(4, question.getAnswerOptions().length);
+
+        } catch (Exception e) {
+            fail("Exception should not be thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCreateQuizFromEmptyJson() {
+        String json = "{}";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            quizImporter.createQuizFromJson(json);
+        });
+    }
+
+    @Test
+    public void testCreateQuizFromJsonWithoutQuestions() {
+        String json = "{\"title\": \"Test Quiz\", \"description\": \"This is a test quiz.\"}";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            quizImporter.createQuizFromJson(json);
+        });
+    }
+
+    @Test
+    public void testCreateQuizFromJsonWithInvalidCorrectAnswer() {
+        String json = "{\"title\": \"Test Quiz\", \"description\": \"This is a test quiz.\", \"questions\": [{\"title\": \"Test Question\", \"correctAnswer\": \"Z\", \"answerOptions\": [\"Option A\", \"Option B\", \"Option C\", \"Option D\"]}]}";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            quizImporter.createQuizFromJson(json);
+        });
+    }
+
+    @Test
+    public void testCreateQuizFromJsonWithEmptyQuestion() {
+        String json = "{\"title\": \"Test Quiz\", \"description\": \"This is a test quiz.\", \"questions\": [{}]}";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            quizImporter.createQuizFromJson(json);
+        });
+    }
+}
