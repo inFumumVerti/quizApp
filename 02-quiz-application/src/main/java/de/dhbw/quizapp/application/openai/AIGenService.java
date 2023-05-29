50d1aa1f9e1178c560834a14802bc9d1e8c54928
inFumumVerti <linus.pust@gmail.com>
2023-05-29 11:04:27 +0200

    Refactoring: Cleaning up AIGenService.java

--- 02-quiz-application/src/main/java/de/dhbw/quizapp/application/openai/AIGenService.java
+++ 02-quiz-application/src/main/java/de/dhbw/quizapp/application/openai/AIGenService.java
@@ -33,63 +33,74 @@ public class AIGenService {
         if (openAiService == null) {
             throw new IllegalStateException("API key not set");
         }
+
+        String prompt = createPrompt(quizName, furtherInfo, noOfQuestions, language);
+        List<ChatMessage> messages = createChatMessages(prompt);
+        ChatCompletionRequest chatCompletionRequest = createChatCompletionRequest(messages);
+
+        return openAiService.createChatCompletion(chatCompletionRequest).getChoices().get(0).getMessage().getContent();
+    }
+
+    private String createPrompt(String quizName, String furtherInfo, int noOfQuestions, String language) {
         String promptTemplate = """
-    Create a quiz using the following information:
-    {
-        "title": "%s",
-        "description": "%s",
-        "number of questions": "%d",
-        "language of questions": "%s",
-        "example question and answer": "Example question: <exampleQuestion>, Example answer: <exampleAnswer>"
+Create a quiz using the following information:
+{
+    "title": "%s",
+    "description": "%s",
+    "number of questions": "%d",
+    "language of questions": "%s",
+    "example question and answer": "Example question: <exampleQuestion>, Example answer: <exampleAnswer>"
+}
+Use this exact format:
+{
+    "title": "<QuizName>",
+    "description": "<description>",
+    "questions": [
+        {
+            "title": "<question1>",
+            "correctAnswer": "<Correct Answer = A | B | C | D>",
+            "answerOptions": [
+                "<answerOption1.A>, <answerOption1.B>, <answerOption1.C>, <answerOption1.D>"
+            ]
+        },
+        // more questions...
+    ]
+}
+
+Example:
+
+Quiz in JSON-Format:
+{
+    "title": "European Capitals",
+    "description": "A quiz to test knowledge of the capitals of various European countries.",
+    "questions": [
+        {
+            "title": "What is the capital of Germany?",
+            "correctAnswer": "C",
+            "answerOptions": ["Paris", "London", "Berlin", "Rome"]
+        },
+        // more questions...
+    ]
+}
+""";
+        return String.format(promptTemplate, quizName, furtherInfo, noOfQuestions, language);
     }
-    "Use this exact format:\\n" +
-    "{
-" +
-    "    \\"title\\": \\"<QuizName>\\",\\n" +
-    "    \\"description\\": \\"<description>\\",\\n" +
-    "    \\"questions\\": [\\n" +
-    "        {\\n" +
-    "            \\"title\\": \\"<question1>\\",\\n" +
-    "            \\"correctAnswer\\": \\"<Correct Answer = A | B | C | D>\\",\\n" +
-    "            \\"answerOptions\\": [\\n" +
-    "                \\"<answerOption1.A>, <answerOption1.B>, <answerOption1.C>, <answerOption1.D>\\"\\n" +
-    "            ]\\n" +
-    "        },\\n" +
-    "        // more questions...\\n" +
-    "    ]\\n" +
-    "}";
 
-    Example:
-    
-    Quiz in JSON-Format:
-    {
-        "title": "European Capitals",
-        "description": "A quiz to test knowledge of the capitals of various European countries.",
-        "questions": [
-            {
-                "title": "What is the capital of Germany?",
-                "correctAnswer": "C",
-                "answerOptions": ["Paris", "London", "Berlin", "Rome"]
-            },
-            // more questions...
-        ]
-    }
-    """;
-
-        String prompt = String.format(promptTemplate, quizName, furtherInfo, noOfQuestions, language);
-
+    private List<ChatMessage> createChatMessages(String prompt) {
         List<ChatMessage> messages = new ArrayList<>();
         ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), prompt);
         messages.add(systemMessage);
 
-        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+        return messages;
+    }
+
+    private ChatCompletionRequest createChatCompletionRequest(List<ChatMessage> messages) {
+        return ChatCompletionRequest
                 .builder()
                 .model("gpt-3.5-turbo")
                 .messages(messages)
                 .n(1)
                 .maxTokens(500)
                 .build();
-
-        return openAiService.createChatCompletion(chatCompletionRequest).getChoices().get(0).getMessage().getContent();
     }
 }
